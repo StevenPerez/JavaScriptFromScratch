cf43de9e0e5efcc65372ccad4a4767edb45c99f2
Steven <perez.s.19>
2015-06-16 10:11:01 -0600

    Advance Loops

--- src/basic.js
+++ src/basic.js
@@ -76,15 +76,66 @@ var d = d || 'New assignment';
 console.log(d);
 */
 
+// 3. Objects
+/*
+var person = {
+    name: null,
+    age: null
+};
+
+console.log(typeof person);
+
+person.name = 'Steven';
+person['age'] = 29;
+
+console.log(person);
+
+console.log(person.name);
+console.log(person['age']);
+
+console.log( person.hasOwnProperty('name') );
+*/
+
+// 4. Arrays
+
+var cars = [];
+
+console.log(typeof cars);
+console.log( Array.isArray(cars) );
+
+// Create
+cars.push( { name: 'Tesla', model: 2015, ranking: 10 } );
+cars.push( { name: 'Eclipse', model: 2014, ranking: 8 } );
+cars.push( { name: 'Mazda', model: 2012, ranking: 7 } );
+cars.push( { name: 'Toyota', model: 2013, ranking: 6 } );
+
+// Read
+console.log(cars);
+console.log(cars[2].name);
+console.log(cars[2]['model']);
+
+// Update
+console.log(cars[2]);
+cars[2].model = 2015;
+console.log(cars[2]);
+
+// Delete
+
+var index = cars.map(function(car) { return car.name; }).indexOf('Mazda');
+console.log(index);
+
+if (index > -1)
+    cars.splice(index, 1);
+
+console.log(cars);
 
 
+// 4.2 Advanced Loops IE+9
 
-
-
-
-
-
-
+// forEach
+cars.forEach(function(car){
+    console.log( car.name );
+});
 
 
 
