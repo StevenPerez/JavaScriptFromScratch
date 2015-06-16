be61c19f1fd424be6be781ac4a97335907b974c0
Steven <perez.s.19>
2015-06-16 09:08:43 -0600

    IF Conditions

--- src/basic.js
+++ src/basic.js
@@ -3,7 +3,7 @@
 Multiple
 lines
 */
-
+/*
 // string
 var a = 'home';
 console.log(typeof a);
@@ -11,3 +11,88 @@ console.log(typeof a);
 // number
 var b = 1;
 console.log(typeof b);
+
+// undefined
+console.log(typeof c);
+
+// string 2
+var n = '1';
+console.log(typeof n);
+
+var g = b + n;
+console.log(g);
+
+// 1.2 Type Convertions
+
+n = Number(n);
+console.log(typeof n);
+
+b = b.toString();
+console.log(typeof b);
+
+b = parseInt(b);
+console.log(typeof b);
+
+b = parseFloat(b);
+console.log(typeof b);
+*/
+
+
+// 2. IF Conditions
+/*
+var a = '1', b = 1, c = null;
+
+if (a === b) {
+    // Yes
+    console.log('Same value');
+} else {
+    // No
+    console.log('Different value');
+}
+
+// AND = &&
+if (a == b && c == null)
+    console.log('Perfect key');
+else
+    console.log('No perfect key');
+
+// OR = ||
+if (c != null || a == '1')
+    console.log('At least one key matched');
+
+
+var input = '1';
+if (a === input)
+    console.log('Match with a');
+else if (b === input)
+    console.log('Match with b');
+else
+    console.log('NA');
+
+var res = (a == '1') ? 'Yes' : 'No';
+console.log(res);
+
+var d = d || 'New assignment';
+console.log(d);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
